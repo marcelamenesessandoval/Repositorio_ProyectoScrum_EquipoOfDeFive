29b20b0cf940f48ac4d051a3582aee01bc1e3b6f
David Puerto <dsp5502@gmail.com>
2021-10-02 18:40:44 -0500

    agregar pcorreciones a la pagina ingresar producto falta organizar estilos

--- src/pages/admin/productos/Ingresar.jsx
+++ src/pages/admin/productos/Ingresar.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 
 const IngresarPage = () => {
     return (
-        
-            <section className="form-registro">
+        <section className="form-registro">
                 <form>
                     <h4>Registro de Productos</h4>
                     <input className="controls" type="number" name="ID (Inmutable)" placeholder="Identificador unico" max="9999" required />
                     <input className="controls" type="text" name="Nombre Producto" placeholder="Nombre del Producto" required />
                     <input className="controls descripcion" type="text" name="Descripcion" placeholder="Añadir una descripcion del producto" required />
                     <input className="controls " type="number" name="Valor Unitario" placeholder="Valor Unitario" required />
-                    <select className="controls mouse" required  >
-                        <option selected disabled hidden>Estado del Producto</option>
+                    <select className="controls mouse" required defaultValue={0}  >
+                        <option  disabled value={0} >Estado del Producto</option>
                         <option >Disponible</option>
                         <option >No Disponible</option>
                     </select>
@@ -22,7 +21,6 @@ const IngresarPage = () => {
 
 
             </section>
-        
     );
 }
 
