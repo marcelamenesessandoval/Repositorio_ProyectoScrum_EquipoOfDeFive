ccc56b420dd8c3b525ca41850fe3042a30c448a3
marcelamenesessandoval <marchelsay4gmail.com>
2021-10-10 18:53:24 -0500

    Subiendo actualización de las tablas, con el buscador con funcionalidad y filas con color impares

--- src/pages/admin/productos.jsx
+++ src/pages/admin/productos.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Dialog from '@mui/material/Dialog';
+import { nanoid } from 'nanoid';
+
+
 
 const arregloProductosProvisional = [
     {
@@ -39,43 +43,27 @@ const ProductosPage = () => {
     }, [])
 
     return (
-        <div className='backgroundTables'>
-            <section className="styleContentSection">
+        <div className="styleContentSection">
 
-                <h2 className="tableTitle">Gestión de productos</h2>
-                <p className="textTable">A continuación, se enseña el listado de productos registrados. Para modificar la información de productos, pulse sobre el ícono editar.</p>
-                <br />
-                <p className="textTable">Para registrar un nuevo producto, pulse el botón <b>Registrar producto.</b></p>
+            <h2 className="tableTitle">Gestión de productos</h2>
+            <p className="textTable">A continuación, se enseña el listado de productos registrados. Para registrar un nuevo producto, pulse el botón <b>Registrar producto.</b></p>
 
-                <section className="styleOptionsTable">
-                    <Link to="/admin/productos/ingresar">
-                        <button className="buttonCreate">Registrar producto</button>
-                    </Link>
-                    <div className="searchInTable">
-                        <label className="labelSearch" htmlFor="buscar">Buscar: </label>
-                        <input className="inputSearch" type="text" />
-                    </div>
-                </section>
+            <TablaProductos listaProductos={productos} />
 
-                <TablaProductos listaProductos={productos} />
-
-                <section className="pager">
-                    <div className="numPag">
-                        <i className="fas fa-chevron-left"></i>
-                    </div>
-                    <div className="numPag">1</div>
-                    <div className="numPag">2</div>
-                    <div className="numPag">3</div>
-                    <div className="numPag">4</div>
-                    <div className="numPag">5</div>
-                    <div className="numPag">
-                        <i className="fas fa-chevron-right"></i>
-                    </div>
-
-                </section>
+            <section className="pager">
+                <div className="numPag">
+                    <i className="fas fa-chevron-left"></i>
+                </div>
+                <div className="numPag">1</div>
+                <div className="numPag">2</div>
+                <div className="numPag">3</div>
+                <div className="numPag">4</div>
+                <div className="numPag">5</div>
+                <div className="numPag">
+                    <i className="fas fa-chevron-right"></i>
+                </div>
 
             </section>
-
         </div>
     );
 
@@ -83,26 +71,58 @@ const ProductosPage = () => {
 
 
 const TablaProductos = ({ listaProductos }) => {
+    const [openDialog, setOpenDialog] = useState(false);
+    const [busqueda, setBusqueda] = useState('');
+    const [productosFiltrados, setProductosFiltrados] = useState(listaProductos);
+
     useEffect(() => {
         console.log("Este es el listado de productos", listaProductos)
     }, [listaProductos]);
 
+
+
+    useEffect(() => {
+        console.log('Búsqueda', busqueda);
+        console.log('Lista de productos', arregloProductosProvisional);
+        setProductosFiltrados(
+            listaProductos.filter((elemento) => {
+                console.log("Elemento", elemento);
+                return JSON.stringify(elemento).toLowerCase().includes(busqueda.toLowerCase());
+            })
+        );
+    }, [busqueda, listaProductos]);
+
+
+
     return (
         <>
+            <section className="styleOptionsTable">
+                <Link to="/admin/productos/ingresar">
+                    <button className="buttonCreate">Registrar producto</button>
+                </Link>
+                <div className="searchInTable">
+                    <label className="labelSearch" htmlFor="buscar">Buscar: </label>
+                    <input
+                        value={busqueda}
+                        onChange={(e) => setBusqueda(e.target.value)}
+                        className="inputSearch" type="text" />
+                </div>
+            </section>
+
             <table className="table">
                 <thead>
                     <tr className="fila">
                         <th className="column narrowColumn">ID</th>
                         <th className="column">PRODUCTO</th>
                         <th className="column widthColumn">DESCRIPCIÓN</th>
-                        <th className="column">OPCIONES</th>
+                        <th className="column iconColumn">OPCIONES</th>
                     </tr>
                 </thead>
 
                 <tbody>
-                    {listaProductos.map((producto) => {
+                    {productosFiltrados.map((producto) => {
                         return (
-                            <tr className="row">
+                            <tr key={nanoid} className="row">
                                 <td className="cell">{producto.id}</td>
                                 <td className="cell">{producto.producto}</td>
                                 <td className="cell">{producto.descripcion}</td>
@@ -111,10 +131,30 @@ const TablaProductos = ({ listaProductos }) => {
                                         <span className="tooltipText">Detalles</span>
                                     </i>
                                     <Link to="/admin/productos/editar">
-                                    <i className="far fa-edit editIcon tooltip">
-                                        <span className="tooltipText">Editar</span>
-                                    </i>
+                                        <i className="far fa-edit editIcon tooltip">
+                                            <span className="tooltipText">Editar</span>
+                                        </i>
                                     </Link>
+
+
+                                    <i class="fas fa-trash-alt deleteIcon tooltip"
+                                        onClick={() => setOpenDialog(true)}>
+                                        <span className="tooltipText">Eliminar</span>
+                                    </i>
+
+                                    <Dialog fullWidth maxWidth="sm" className="dialog" open={openDialog}>
+                                        <h3 className="titleDialog">Confirmar eliminación</h3>
+                                        <div>
+                                            <p className="textDelete">¿Está seguro de eliminar el producto?</p>
+                                        </div>
+                                        <div className="divButtonDialog">
+                                            <button className="buttonDialogDelete">Sí, eliminar</button>
+                                            <button
+                                                onClick={() => setOpenDialog(false)}
+                                                className="buttonDialogDelete">Cancelar</button>
+                                        </div>
+                                    </Dialog>
+
                                 </td>
                             </tr>
 
--- src/pages/admin/usuarios.jsx
+++ src/pages/admin/usuarios.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Dialog from '@mui/material/Dialog';
+import { nanoid } from 'nanoid';
+
 
 const arregloUsuariosProvisional = [
     {
@@ -44,34 +47,25 @@ const UsuariosPage = () => {
     }, [])
 
     return (
-        <div className='backgroundTables'>
-            <section className="styleContentSection">
+        <div className="styleContentSection">
 
-                <h2 className="tableTitle">Gestión de usuarios</h2>
-                <p className="textTable">A continuación, se enseña el listado de usuarios registrados. Para modificar la información de usuarios, pulse sobre el ícono editar.</p>
+            <h2 className="tableTitle">Gestión de usuarios</h2>
+            <p className="textTable">A continuación, se enseña el listado de usuarios registrados.</p>
 
-                <section className="styleOptionsTable">
-                    <div className="searchInTableUser">
-                        <label className="labelSearch" htmlFor="buscar">Buscar: </label>
-                        <input className="inputSearch" type="text" />
-                    </div>
-                </section>
-                <TablaUsuarios listaUsuarios={usuarios} />
+            <TablaUsuarios listaUsuarios={usuarios} />
 
-                <section className="pager">
-                    <div className="numPag">
-                        <i className="fas fa-chevron-left"></i>
-                    </div>
-                    <div className="numPag">1</div>
-                    <div className="numPag">2</div>
-                    <div className="numPag">3</div>
-                    <div className="numPag">4</div>
-                    <div className="numPag">5</div>
-                    <div className="numPag">
-                        <i className="fas fa-chevron-right"></i>
-                    </div>
-
-                </section>
+            <section className="pager">
+                <div className="numPag">
+                    <i className="fas fa-chevron-left"></i>
+                </div>
+                <div className="numPag">1</div>
+                <div className="numPag">2</div>
+                <div className="numPag">3</div>
+                <div className="numPag">4</div>
+                <div className="numPag">5</div>
+                <div className="numPag">
+                    <i className="fas fa-chevron-right"></i>
+                </div>
 
             </section>
 
@@ -82,12 +76,41 @@ const UsuariosPage = () => {
 
 
 const TablaUsuarios = ({ listaUsuarios }) => {
+    const [openDialog, setOpenDialog] = useState(false);
+    const [busqueda, setBusqueda] = useState('');
+    const [usuariosFiltrados, setUsariosFiltrados] = useState(listaUsuarios);
+
+
     useEffect(() => {
         console.log("Este es el listado de usuarios", listaUsuarios)
     }, [listaUsuarios]);
 
+
+    useEffect(() => {
+        console.log('Búsqueda', busqueda);
+        console.log('Lista de usuarios', arregloUsuariosProvisional);
+        setUsariosFiltrados(
+            listaUsuarios.filter((elemento) => {
+                console.log("Elemento", elemento);
+                return JSON.stringify(elemento).toLowerCase().includes(busqueda.toLowerCase());
+            })
+        );
+    }, [busqueda, listaUsuarios]);
+
+
     return (
         <>
+
+            <section className="styleOptionsTable">
+                <div className="searchInTableUser">
+                    <label className="labelSearch" htmlFor="buscar">Buscar: </label>
+                    <input
+                        value={busqueda}
+                        onChange={(e) => setBusqueda(e.target.value)}
+                        className="inputSearch" type="text" />
+                </div>
+            </section>
+
             <table className="table">
                 <thead>
                     <tr className="fila">
@@ -95,13 +118,13 @@ const TablaUsuarios = ({ listaUsuarios }) => {
                         <th className="column widthColumn">NOMBRE USUARIO</th>
                         <th className="column">ROL</th>
                         <th className="column ">ESTADO</th>
-                        <th className="column">OPCIONES</th>
+                        <th className="column iconColumn">OPCIONES</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {listaUsuarios.map((usuario) => {
+                    {usuariosFiltrados.map((usuario) => {
                         return (
-                            <tr className="row">
+                            <tr key={nanoid} className="row">
                                 <td className="cell">{usuario.id}</td>
                                 <td className="cell">{usuario.nombre}</td>
                                 <td className="cell">{usuario.rol}</td>
@@ -115,6 +138,25 @@ const TablaUsuarios = ({ listaUsuarios }) => {
                                             <span className="tooltipText">Editar</span>
                                         </i>
                                     </Link>
+
+                                    <i class="fas fa-trash-alt deleteIcon tooltip"
+                                        onClick={() => setOpenDialog(true)}>
+                                        <span className="tooltipText">Eliminar</span>
+                                    </i>
+
+                                    <Dialog fullWidth maxWidth="sm" className="dialog" open={openDialog}>
+                                        <h3 className="titleDialog">Confirmar eliminación</h3>
+                                        <div>
+                                            <p className="textDelete">¿Está seguro de eliminar el usuario?</p>
+                                        </div>
+                                        <div className="divButtonDialog">
+                                            <button className="buttonDialogDelete">Sí, eliminar</button>
+                                            <button
+                                                onClick={() => setOpenDialog(false)}
+                                                className="buttonDialogDelete">Cancelar</button>
+                                        </div>
+                                    </Dialog>
+
                                 </td>
                             </tr>
                         );
