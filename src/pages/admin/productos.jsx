02a9f1f3a9d919316b97d3114842627ece3d04db
marcelamenesessandoval <marchelsay4gmail.com>
2021-10-14 15:48:52 -0500

    Subiendo cambios sobre el módulo de productos, pero aún está en proceso de elaboración

--- src/pages/admin/productos.jsx
+++ src/pages/admin/productos.jsx
@@ -1,77 +1,118 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState, useRef } from "react";
 import Dialog from '@mui/material/Dialog';
 import { nanoid } from 'nanoid';
+import axios from "axios";
 
 
 
-const arregloProductosProvisional = [
-    {
-        id: "p001",
-        producto: "Producto 1",
-        descripcion: "Producto con características de prueba.",
-    },
-    {
-        id: "p002",
-        producto: "Producto 2",
-        descripcion: "Producto con características de prueba.",
-    },
-    {
-        id: "p003",
-        producto: "Producto 3",
-        descripcion: "Producto con características de prueba.",
-    },
-    {
-        id: "p004",
-        producto: "Producto 4",
-        descripcion: "Producto con características de prueba.",
-    },
-    {
-        id: "p005",
-        producto: "Producto 3",
-        descripcion: "Producto con características de prueba.",
-    }
-];
+// const arregloProductosProvisional = [
+//     {
+//         id: "p001",
+//         producto: "Producto 1",
+//         descripcion: "Producto con características de prueba.",
+//         valorUnitario: 30000,
+//         estado: "Disponible",
+//     },
+//     {
+//         id: "p002",
+//         producto: "Producto 2",
+//         descripcion: "Producto con características de prueba.",
+//         valorUnitario: 22000,
+//         estado: "Disponible",
+//     },
+//     {
+//         id: "p003",
+//         producto: "Producto 3",
+//         descripcion: "Producto con características de prueba.",
+//         valorUnitario: 76000,
+//         estado: "Disponible",
+//     },
+//     {
+//         id: "p004",
+//         producto: "Producto 4",
+//         descripcion: "Producto con características de prueba.",
+//         valorUnitario: 30000,
+//         estado: "Disponible",
+//     },
+//     {
+//         id: "p005",
+//         producto: "Producto 3",
+//         descripcion: "Producto con características de prueba.",
+//         valorUnitario: 30000,
+//         estado: "Disponible",
+//     }
+// ];
 
 
 
 const ProductosPage = () => {
     const [productos, setProductos] = useState([]);
+    const [mostrarTabla, setMostrarTabla] = useState(true);
+    const [textoBoton, setTextoBoton] = useState('');
+    const [ejecutarConsulta, setEjecutarConsulta] = useState(true);
+
+
+    const obtenerProductos = async () => {
+        const options = { method: 'GET', url: '' };
+        await axios
+            .request(options)
+            .then(function (response) {
+                setProductos(response.data);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+        setEjecutarConsulta(false);
+    };
+
+
 
     useEffect(() => {
-        setProductos(arregloProductosProvisional);
-    }, [])
+        console.log('consulta', ejecutarConsulta);
+        if (ejecutarConsulta) {
+            obtenerProductos();
+        }
+    }, [ejecutarConsulta]);
+
+    useEffect(() => {
+        if (mostrarTabla) {
+            setEjecutarConsulta(true);
+        }
+    }, [mostrarTabla]);
+
+
+    useEffect(() => {
+        if (mostrarTabla) {
+            setTextoBoton('Registrar producto');
+        } else {
+            setTextoBoton('Regresar');
+        }
+    }, [mostrarTabla]);
+
+
+
 
     return (
         <div className="styleContentSection">
 
-            <h2 className="tableTitle">Gestión de productos</h2>
-            <p className="textTable">A continuación, se enseña el listado de productos registrados. Para registrar un nuevo producto, pulse el botón <b>Registrar producto.</b></p>
+            {mostrarTabla ?
+                <TablaProductos listaProductos={productos}
+                    setAgregarProducto={setEjecutarConsulta} /> :
+                <IngresarPage
+                    setMostrarTabla={setMostrarTabla}
+                    listaProductos={productos}
+                    setAgregarProducto={setProductos} />
+            };
 
-            <TablaProductos listaProductos={productos} />
-
-            <section className="pager">
-                <div className="numPag">
-                    <i className="fas fa-chevron-left"></i>
-                </div>
-                <div className="numPag">1</div>
-                <div className="numPag">2</div>
-                <div className="numPag">3</div>
-                <div className="numPag">4</div>
-                <div className="numPag">5</div>
-                <div className="numPag">
-                    <i className="fas fa-chevron-right"></i>
-                </div>
-
-            </section>
+            <button onClick={() => { setMostrarTabla(!mostrarTabla) }} className="buttonCreate">{textoBoton}</button>
         </div>
     );
 
 }
 
 
-const TablaProductos = ({ listaProductos }) => {
-    const [openDialog, setOpenDialog] = useState(false);
+const TablaProductos = ({ listaProductos, setEjecutarConsulta }) => {
+
     const [busqueda, setBusqueda] = useState('');
     const [productosFiltrados, setProductosFiltrados] = useState(listaProductos);
 
@@ -83,7 +124,6 @@ const TablaProductos = ({ listaProductos }) => {
 
     useEffect(() => {
         console.log('Búsqueda', busqueda);
-        console.log('Lista de productos', arregloProductosProvisional);
         setProductosFiltrados(
             listaProductos.filter((elemento) => {
                 console.log("Elemento", elemento);
@@ -96,10 +136,14 @@ const TablaProductos = ({ listaProductos }) => {
 
     return (
         <>
+
+            <h2 className="tableTitle">Gestión de productos</h2>
+            <p className="textTable">A continuación, se enseña el listado de productos registrados. Para registrar un nuevo producto, pulse el botón <b>Registrar producto.</b></p>
+
             <section className="styleOptionsTable">
-                <Link to="/admin/productos/ingresar">
+                {/* <Link to="/admin/productos/ingresar">
                     <button className="buttonCreate">Registrar producto</button>
-                </Link>
+                </Link> */}
                 <div className="searchInTable">
                     <label className="labelSearch" htmlFor="buscar">Buscar: </label>
                     <input
@@ -121,53 +165,226 @@ const TablaProductos = ({ listaProductos }) => {
 
                 <tbody>
                     {productosFiltrados.map((producto) => {
-                        return (
-                            <tr key={nanoid} className="row">
-                                <td className="cell">{producto.id}</td>
-                                <td className="cell">{producto.producto}</td>
-                                <td className="cell">{producto.descripcion}</td>
-                                <td className="cell">
-                                    <i className="far fa-eye detailIcon tooltip">
-                                        <span className="tooltipText">Detalles</span>
-                                    </i>
-                                    <Link to="/admin/productos/editar">
-                                        <i className="far fa-edit editIcon tooltip">
-                                            <span className="tooltipText">Editar</span>
-                                        </i>
-                                    </Link>
-
-
-                                    <i class="fas fa-trash-alt deleteIcon tooltip"
-                                        onClick={() => setOpenDialog(true)}>
-                                        <span className="tooltipText">Eliminar</span>
-                                    </i>
-
-                                    <Dialog fullWidth maxWidth="sm" className="dialog" open={openDialog}>
-                                        <h3 className="titleDialog">Confirmar eliminación</h3>
-                                        <div>
-                                            <p className="textDelete">¿Está seguro de eliminar el producto?</p>
-                                        </div>
-                                        <div className="divButtonDialog">
-                                            <button className="buttonDialogDelete">Sí, eliminar</button>
-                                            <button
-                                                onClick={() => setOpenDialog(false)}
-                                                className="buttonDialogDelete">Cancelar</button>
-                                        </div>
-                                    </Dialog>
-
-                                </td>
-                            </tr>
-
-                        );
+                        return <FilaProducto key={nanoid} producto={producto} setEjecutarConsulta={setEjecutarConsulta} />;
                     })}
 
                 </tbody>
             </table>
 
+            <section className="pager">
+                <div className="numPag">
+                    <i className="fas fa-chevron-left"></i>
+                </div>
+                <div className="numPag">1</div>
+                <div className="numPag">2</div>
+                <div className="numPag">3</div>
+                <div className="numPag">4</div>
+                <div className="numPag">5</div>
+                <div className="numPag">
+                    <i className="fas fa-chevron-right"></i>
+                </div>
+            </section>
+
         </>
     )
 
 }
+
+
+const FilaProducto = (setMostrarTabla, producto, setEjecutarConsulta) => {
+    const [openDialog, setOpenDialog] = useState(false);
+    const [edit, setEdit] = useState(false);
+
+    if (!edit) {
+        EditProducto(producto, setMostrarTabla);
+    }
+
+    const eliminarProducto = async () => {
+        const options = {
+            method: 'DELETE',
+            url: '',
+            headers: { 'Content-Type': 'application/json' },
+            data: { id: producto._id },
+        };
+
+
+        await axios
+            .request(options)
+            .then(function (response) {
+                console.log(response.data);
+                alert("El producto se ha eliminado correctamente");
+                setEjecutarConsulta(true);
+     
+            })
+            .catch(function (error) {
+                console.error(error);
+                alert("Hubo un error al eliminar el producto");
+            });
+           setOpenDialog(false);
+    };
+
+    return (
+
+        <tr className="row">
+            <td className="cell">{producto.id}</td>
+            <td className="cell">{producto.producto}</td>
+            <td className="cell">{producto.descripcion}</td>
+            <td className="cell">
+                <i className="far fa-eye detailIcon tooltip">
+                    <span className="tooltipText">Detalles</span>
+                </i>
+
+                <i className="far fa-edit editIcon tooltip"
+                    onClick={() => setEdit(!edit)}>
+                    <span className="tooltipText">Editar</span>
+                </i>
+
+
+                <i class="fas fa-trash-alt deleteIcon tooltip"
+                    onClick={() => setOpenDialog(true)}>
+                    <span className="tooltipText">Eliminar</span>
+                </i>
+
+                <Dialog fullWidth maxWidth="sm" className="dialog" open={openDialog}>
+                    <h3 className="titleDialog">Confirmar eliminación</h3>
+                    <div>
+                        <p className="textDelete">¿Está seguro de eliminar el producto?</p>
+                    </div>
+                    <div className="divButtonDialog">
+                        <button
+                            onClick={() => eliminarProducto()}
+                            className="buttonDialogDelete">Sí, eliminar
+                        </button>
+                        <button
+                            onClick={() => setOpenDialog(false)}
+                            className="buttonDialogDelete">Cancelar
+                        </button>
+                    </div>
+                </Dialog>
+
+            </td>
+        </tr>
+
+    )
+};
+
+
+
+
+const IngresarPage = ({ setMostrarTabla, listaProductos, setAgregarProducto }) => {
+
+    const form = useRef(null);
+
+    const submitForm = async (e) => {
+        e.preventDefault();
+        const fd = new FormData(form.current);
+        const nuevoProducto = {};
+        fd.forEach((value, key) => {
+            nuevoProducto[key] = value;
+        });
+
+
+        const options = {
+            method: 'POST',
+            url: '',
+            headers: { 'Content-Type': 'application/json' },
+            data: { id: nuevoProducto.id, producto: nuevoProducto.producto, descripcion: nuevoProducto.descripcion, valorUnitario: nuevoProducto.valorUnitario, estado: nuevoProducto.estado },
+        };
+
+
+        await axios
+            .request(options)
+            .then(function (response) {
+                console.log(response.data);
+                alert("El producto se ha añadido correctamente");
+            })
+            .catch(function (error) {
+                console.error(error);
+                alert("Hubo un error al guardar el producto");
+            });
+
+
+        setMostrarTabla(true)
+
+    };
+
+
+
+
+    return (
+        <div className="fondo">
+            <section className="form-registro">
+                <form ref={form} onSubmit={submitForm}>
+                    <h4>Registro de Producto</h4>
+                    <input className="controls" type="number" name="id" placeholder="Identificador unico" max="9999" required />
+                    <input className="controls" type="text" name="producto" placeholder="Nombre del Producto" required />
+                    <input className="controls descripcion" type="text" name="descripcion" placeholder="Añadir una descripcion del producto" required />
+                    <input className="controls " type="number" name="valorUnitario" placeholder="Valor Unitario" required />
+                    <select className="controls mouse" name="estado" defaultValue={0} required>
+                        <option disabled value={0}>Estado del Producto</option>
+                        <option >Disponible</option>
+                        <option >No Disponible</option>
+                    </select>
+                    {/* Botón provisional luego agregar el alert*/}
+                    <button className="botonRegistro" type="submit">
+                        Enviar dos
+                    </button>
+                    {/* <input className="botonRegistro" type="submit" onClick={() => alert("El producto se ha añadido correctamente")} defaultValue="Enviar" /> */}
+                </form>
+
+
+            </section>
+        </div>
+    );
+}
+
+
+
+
+const EditProducto = ({ producto, setMostrarTabla }) => {
+
+    const form = useRef(null);
+
+    const submitForm = async (e) => {
+        e.preventDefault();
+        const fd = new FormData(form.current);
+        const nuevoProducto = {};
+        fd.forEach((value, key) => {
+            nuevoProducto[key] = value;
+        });
+
+
+
+        return (
+            <div className="fondo">
+                <section className="form-registro">
+                    <form ref={form} onSubmit={submitForm}>
+                        <h4>Editar producto</h4>
+                        <input className="controls" type="number" name="id" placeholder="Identificador unico" max="9999" required defaultValue={producto.id} />
+                        <input className="controls" type="text" name="producto" placeholder="Nombre del Producto" required defaultValue={producto.producto} />
+                        <input className="controls descripcion" type="text" name="descripcion" placeholder="Añadir una descripcion del producto" required defaultValue={producto.descripcion} />
+                        <input className="controls " type="number" name="valorUnitario" placeholder="Valor Unitario" required defaultValue={producto.valorUnitario} />
+                        <select className="controls mouse" name="estado" defaultValue={0} required defaultValue={producto.estado}>
+                            <option disabled value={0}>Estado del Producto</option>
+                            <option >Disponible</option>
+                            <option >No Disponible</option>
+                        </select>
+                        {/* Botón provisional luego agregar el alert*/}
+                        <button className="botonRegistro" type="submit">
+                            Enviar dos
+                        </button>
+                        {/* <input className="botonRegistro" type="submit" onClick={() => alert("El producto se ha añadido correctamente")} defaultValue="Enviar" /> */}
+                    </form>
+
+
+                </section>
+            </div>
+        );
+    }
+
+}
+
+
 
 
 export default ProductosPage;
