92447dad8ede9f22bb24f8f500055942fc4e3b2f
sergio <gioprogramacionweb@gmail.com>
2021-10-24 17:18:26 -0500

    correccion de rutas

--- src/pages/registro.jsx
+++ src/pages/registro.jsx
@@ -1,38 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
 
-const RegistroPage = () => {
-    return <div>
-        <div className="registro">
+// const RegistroPage = () => {
+//     return <div>
+//         <div className="registro">
 
-            <h1> Registro usuarios</h1>
+//             <h1> Registro usuarios</h1>
 
-            <form>
-                <label for="Username">Ingrese su correo electronico</label>
-                <input type="email" />
+//             <form>
+//                 <label for="Username">Ingrese su correo electronico</label>
+//                 <input type="email" />
 
-                <label for="Nombres">Ingrese sus nombres </label>
-                <input type="text" />
+//                 <label for="Nombres">Ingrese sus nombres </label>
+//                 <input type="text" />
 
-                <label for="Apellidos">Ingrese sus apellidos </label>
-                <input type="text" />
+//                 <label for="Apellidos">Ingrese sus apellidos </label>
+//                 <input type="text" />
 
-                <label for="password">Ingrese su Contraseña</label>
-                <input type="password" />
+//                 <label for="password">Ingrese su Contraseña</label>
+//                 <input type="password" />
 
-                <input type="submit" value="Registrarse" />
+//                 <input type="submit" value="Registrarse" />
 
-                <p>¿Ya tienes una cuenta?</p>
-                <br></br>
-                <br></br>
-                <br></br>
-                <Link to='/'>
-                    <p>Volver al inicio</p>
-                </Link>
+//                 <p>¿Ya tienes una cuenta?</p>
+//                 <br></br>
+//                 <br></br>
+//                 <br></br>
+//                 <Link to='/'>
+//                     <p>Volver al inicio</p>
+//                 </Link>
 
-            </form>
-        </div>
-    </div>;
-};
+//             </form>
+//         </div>
+//     </div>;
+// };
 
-export default RegistroPage;
+// export default RegistroPage;
