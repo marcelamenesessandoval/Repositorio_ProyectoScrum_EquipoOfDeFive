763393ff87569eca18c3cedaa025aecd7264f0b8
sergio <gioprogramacionweb@gmail.com>
2021-10-24 19:15:26 -0500

    Funcion onclick mas useAuth0 en botonsidebar

--- src/components/botonlogout.jsx
+++ src/components/botonlogout.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-//import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Botonlogout = () => {
-  //const {logout} = useAuth0()
+  const {logout} = useAuth0()
 
   return (
     <button onClick={() => logout()} type="button">
